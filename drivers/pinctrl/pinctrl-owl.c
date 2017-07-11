52d6b8337a31267fd9b158d5f1f9d9590a16f92d
wowo <wowo@wowotech.net>
2017-07-11 22:07:10 +0800

    [xprj] pinctrl driver: add pinmux ops

--- drivers/pinctrl/pinctrl-owl.c
+++ drivers/pinctrl/pinctrl-owl.c
@@ -20,12 +20,37 @@
 #include <linux/platform_device.h>
 #include <linux/of_device.h>
 
+#include <asm/io.h>
+
 #include <linux/pinctrl/pinctrl.h>
+#include <linux/pinctrl/pinmux.h>
+
+#include "core.h"
+
+struct owl_pinctrl_priv {
+	void __iomem *membase;
+};
+
+struct owl_pmx_reginfo {
+	const unsigned *offset;
+	const unsigned *mask;
+	const unsigned *val;
+
+	unsigned num_entries;
+};
 
 struct owl_pinctrl_group {
 	const char *name;
 	const unsigned *pins;
 	unsigned num_pins;
+
+	const struct owl_pmx_reginfo *reginfo;
+};
+
+struct owl_pmx_func {
+	const char *name;
+	const char * const *groups;
+	unsigned num_groups;
 };
 
 /*============================================================================
@@ -55,21 +80,63 @@ static const unsigned s900_uart5_0_pins[] = { 6, 37 };
 static const unsigned s900_uart5_1_pins[] = { 24, 15 };
 static const unsigned s900_uart5_2_pins[] = { 61, 60 };
 
+/* uart5_0_grp, MFP_CTL1(0xE01B0044)
+ * bit28:26 and bit25:23 equal 001(UART5_RX)
+ * and 100(UART5_TX) */
+static const unsigned s900_uart5_0_offset[] = {0x44, 0x44 };
+static const unsigned s900_uart5_0_mask[] = {0x7 << 26, 0x7 << 23 };
+static const unsigned s900_uart5_0_val[] = {0x1 << 26, 0x4 << 23 };
+static const struct owl_pmx_reginfo s900_uart5_0_reginfo = {
+	.offset	= s900_uart5_0_offset,
+	.mask	= s900_uart5_0_mask,
+	.val	= s900_uart5_0_val,
+	.num_entries = 2,
+};
+
+/* uart5_1_grp, MFP_CTL2(0xE01B0048)
+ * bit19:17 and bit16:14 equal 101(UART5_RX)
+ * and 101(UART5_TX) */
+static const unsigned s900_uart5_1_offset[] = {0x48, 0x48 };
+static const unsigned s900_uart5_1_mask[] = {0x7 << 17, 0x7 << 14 };
+static const unsigned s900_uart5_1_val[] = {0x5 << 17, 0x5 << 14 };
+static const struct owl_pmx_reginfo s900_uart5_1_reginfo = {
+	.offset	= s900_uart5_1_offset,
+	.mask	= s900_uart5_1_mask,
+	.val	= s900_uart5_1_val,
+	.num_entries = 2,
+};
+
+/* uart5_2_grp, MFP_CTL2(0xE01B0048)
+ * bit21 and bit20 equal 1(UART5_RX)
+ * and 1(UART5_TX) */
+static const unsigned s900_uart5_2_offset[] = {0x48, 0x48 };
+static const unsigned s900_uart5_2_mask[] = {0x1 << 21, 0x1 << 20 };
+static const unsigned s900_uart5_2_val[] = {0x1 << 21, 0x1 << 20 };
+static const struct owl_pmx_reginfo s900_uart5_2_reginfo = {
+	.offset	= s900_uart5_2_offset,
+	.mask	= s900_uart5_2_mask,
+	.val	= s900_uart5_2_val,
+	.num_entries = 2,
+};
+
 static const struct owl_pinctrl_group s900_groups[] = {
 	{
 		.name = "uart5_0_grp",
 		.pins = s900_uart5_0_pins,
 		.num_pins = ARRAY_SIZE(s900_uart5_0_pins),
+		.reginfo = &s900_uart5_0_reginfo,
 	},
 	{
 		.name = "uart5_1_grp",
 		.pins = s900_uart5_1_pins,
 		.num_pins = ARRAY_SIZE(s900_uart5_1_pins),
+		.reginfo = &s900_uart5_1_reginfo,
 	},
 	{
 		.name = "uart5_2_grp",
 		.pins = s900_uart5_2_pins,
 		.num_pins = ARRAY_SIZE(s900_uart5_2_pins),
+		.reginfo = &s900_uart5_2_reginfo,
 	},
 };
 
@@ -101,12 +168,82 @@ static struct pinctrl_ops s900_pctrl_ops = {
  * pin groups end
  */
 
+/*
+ * pin multiplex
+ */
+static const char * const uart5_groups[] = {
+	"uart5_0_grp", "uart5_1_grp", "uart5_2_grp"
+};
+
+static const struct owl_pmx_func s900_functions[] = {
+	{
+		.name = "uart5",
+		.groups = uart5_groups,
+		.num_groups = ARRAY_SIZE(uart5_groups),
+	},
+};
+
+static int s900_get_functions_count(struct pinctrl_dev *pctldev)
+{
+	return ARRAY_SIZE(s900_functions);
+}
+
+static const char *s900_get_fname(struct pinctrl_dev *pctldev,
+				  unsigned selector)
+{
+	return s900_functions[selector].name;
+}
+
+static int s900_get_groups(struct pinctrl_dev *pctldev, unsigned selector,
+			   const char * const **groups, unsigned *num_groups)
+{
+	*groups = s900_functions[selector].groups;
+	*num_groups = s900_functions[selector].num_groups;
+
+	return 0;
+}
+
+static int s900_set_mux(struct pinctrl_dev *pctldev, unsigned selector,
+			unsigned group)
+{
+	int i;
+	uint32_t val;
+
+	struct owl_pinctrl_priv *priv;
+
+	const struct owl_pmx_reginfo *reginfo;
+
+	priv = (struct owl_pinctrl_priv *)pctldev->driver_data;
+	reginfo = s900_groups[group].reginfo;
+
+	for (i = 0; i < reginfo->num_entries; i++) {
+		val = readl(priv->membase + reginfo->offset[i]);
+		val &= ~reginfo->mask[i];
+		val |= reginfo->val[i];
+		writel(val, priv->membase + reginfo->offset[i]);
+	}
+
+	return 0;
+}
+
+static struct pinmux_ops s900_pmxops = {
+	.get_functions_count = s900_get_functions_count,
+	.get_function_name = s900_get_fname,
+	.get_function_groups = s900_get_groups,
+	.set_mux = s900_set_mux,
+	.strict = true,
+};
+/*
+ * pin multiplex end
+ */
+
 static const struct pinctrl_desc s900_desc = {
 	.name = "s900",
 	.pins = s900_pins,
 	.npins = ARRAY_SIZE(s900_pins),
 
 	.pctlops = &s900_pctrl_ops,
+	.pmxops = &s900_pmxops,
 
 	.owner = THIS_MODULE,
 };
@@ -127,7 +264,12 @@ MODULE_DEVICE_TABLE(of, owl_pinctrl_of_match);
 static int owl_pinctrl_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
+
 	struct pinctrl_dev *pctl;
+	struct owl_pinctrl_priv *priv;
+
+	struct resource *resource;
+
 	const struct pinctrl_desc *desc;
 
 	dev_info(dev, "%s\n", __func__);
@@ -138,11 +280,30 @@ static int owl_pinctrl_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (priv == NULL) {
+		dev_err(dev, "Failed to allocate memory for priv\n");
+		return -ENOMEM;
+	}
+
+	resource = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!resource) {
+		dev_err(dev, "No IO memory resource\n");
+		return -ENODEV;
+	}
+
+	priv->membase = devm_ioremap_resource(dev, resource);
+	if (IS_ERR(priv->membase)) {
+		dev_err(dev, "Failed to map memory resource\n");
+		return PTR_ERR(priv->membase);
+	}
+
 	pctl = pinctrl_register((struct pinctrl_desc *)desc, dev, NULL);
 	if (!pctl) {
 		dev_err(dev, "could not register pinctrl desc\n");
 		return -EINVAL;
 	}
+	pctl->driver_data = (void *)priv;
 
 	return 0;
 }
